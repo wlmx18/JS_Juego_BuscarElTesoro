0c0b6dc17dd162660329fa7dd735786775ce02cc
wlm.morant <wlm.morant@gmail.com>
2021-01-23 19:20:13 -0500

    ProyectoFinal

--- main.js
+++ main.js
@@ -9,10 +9,14 @@ let target = {
 
 
 let $map = document.getElementById('map')
+let $distance = document.getElementById('distance')
 
 $map.addEventListener('click',function (e) {
     let distance = getDistance(e,target)
     console.log(distance)
-    
+    let distanceHint = getDistanceHint(distance)
+    console.log(distanceHint)
+    //$distance.innerHTML = distanceHint
+    $distance.innerHTML = `<h1> ${distanceHint} </h1>`
 
 })
